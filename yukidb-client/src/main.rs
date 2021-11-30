a51078f6383832641c8804f00e48d34bb92af95d
Taewoo An <taewoo.an@kaist.ac.kr>
2021-11-30 23:32:38 +0900

    Imple simple socket server & client

--- yukidb-client/src/main.rs
+++ yukidb-client/src/main.rs
@@ -1,3 +1,21 @@
+use std::{io::Write, net::TcpStream};
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(about = "simple YukiDB client")]
+struct Opts {
+    #[clap(short, long)]
+    host: String,
+    #[clap(short, long)]
+    port: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+
+    let serveraddr = format!("{}:{}", opts.host, opts.port);
+    let mut conn = TcpStream::connect(serveraddr).expect("Failed to connect to server");
+
+    conn.write(b"Hello, world!").unwrap();
 }
--- yukidb-server/src/main.rs
+++ yukidb-server/src/main.rs
@@ -1,3 +1,35 @@
+use std::{net::TcpListener, io::Read};
+
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(
+    about = "The high performance concurrent key-value in-memory database"
+)]
+struct Opts {
+    #[clap(short, long)]
+    port: u16,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+
+    let serveraddr = format!("0.0.0.0:{}", opts.port);
+    let listener = TcpListener::bind(serveraddr.clone()).expect(&format!("Failed to open server. {}", serveraddr));
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                let mut data: [u8; 50] = [0; 50];
+                match stream.read(&mut data) {
+                    Ok(_) => {
+                        let text = String::from_utf8(data.to_vec()).expect("error on utf8 converting");
+                        println!("from client: {}", text);
+                    },
+                    Err(_) => todo!("err"),
+                }
+            },
+            Err(_) => todo!("?"),
+        }
+    }
 }
